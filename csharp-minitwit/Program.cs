c3f2c6ed6856fa09838f468cb082f1d8d648a756
LVOL98 <lukas98@live.dk>
2024-11-27 05:58:25 +0000

    Added different compilation modes

--- csharp-minitwit/Program.cs
+++ csharp-minitwit/Program.cs
@@ -24,14 +24,14 @@ builder.Host.UseSerilog((context, services, configuration) => configuration
 // Dependency injection
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-// builder.Services.AddDistributedMemoryCache();
+builder.Services.AddDistributedMemoryCache();
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
     options.MinimumSameSitePolicy = SameSiteMode.None;
 });
 
 // Export metrics from all HTTP clients registered in services
-// builder.Services.UseHttpClientMetrics();
+builder.Services.UseHttpClientMetrics();
 
 builder.Services.AddSession(options =>
 {
@@ -52,13 +52,13 @@ builder.Services.AddScoped<IMetaDataRepository, MetaDataRepository>();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllers();
-// builder.Services.AddSwaggerGen(c =>
-// {
-//     // Set the comments path for the Swagger JSON and UI.
-//     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-//     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-//     c.IncludeXmlComments(xmlPath);
-// });
+builder.Services.AddSwaggerGen(c =>
+{
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 // Setup logging to console
 builder.Logging.AddConsole();
@@ -75,17 +75,17 @@ using (var scope = app.Services.CreateScope())
 Log.Information("Starting csharp-minitwit");
 
 // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment() || !app.Environment.IsStaging())
-// {
-//     app.UseExceptionHandler("/Home/Error");
-//     app.UseHsts();
-// }
-// else
-// {
+if (!app.Environment.IsDevelopment() || !app.Environment.IsStaging())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+else
+{
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
-// }
+}
 
 app.UseStaticFiles();
 app.UseRouting();
@@ -93,9 +93,9 @@ app.UseSession();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseCookiePolicy();
-// app.MapMetrics(); // "/metrics"
-// app.UseHttpMetrics();
-// app.UseMiddleware<CatchAllMiddleware>();
+app.MapMetrics(); // "/metrics"
+app.UseHttpMetrics();
+app.UseMiddleware<CatchAllMiddleware>();
 
 app.MapControllerRoute(
     name: "default",
